9b38ea95e07c704f6d812574a257ad472b1efe4c
Melissa Palhano <mlssplhn@gmail.com>
2025-10-04 20:52:03 -0300

    [P1] feat: simple translator and add Parser/App class to math expressions

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String input = "8+5-7+9";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
+    }
+}
